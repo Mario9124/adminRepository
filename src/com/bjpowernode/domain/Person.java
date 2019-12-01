1170c458ed01a1ad7f9a89b0d4a80f84cf983342
sq <sq@nl.com>
2019-12-01 14:32:59 +0800

    初始导入

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,16 @@
+package com.bjpowernode.domain;/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description:
+ *
+ * @date:2019/12/1 14:26
+ * @Author:hua
+ */
+
+/**
+ * Author:19华
+ * 2019/12/1
+ */
+public class Person {
+    private String id;
+}
